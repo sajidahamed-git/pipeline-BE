b18571cf11a12fc7a66494eef14d093141aabe3d
Sajid <sajidahamed2001@gmail.com>
2025-06-19 00:32:49 +0530

    refactor: standardize code formatting and clean up commented-out CORS origins

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const graphlib = require('graphlib')
-const {Graph} = graphlib
+const express = require("express");
+const cors = require("cors");
+const graphlib = require("graphlib");
+const { Graph } = graphlib;
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors({
-  origin: [
-    //'http://localhost:3000',
-     //'http://127.0.0.1:3000'
-     '*'
-    ],
-  //credentials: true
-}));
+app.use(
+  cors({
+    origin: ["*"],
+  })
+);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ Ping: 'Pong' });
+app.get("/", (req, res) => {
+  res.json({ Ping: "Pong" });
 });
 
-app.post('/pipelines/parse', (req, res) => {
+app.post("/pipelines/parse", (req, res) => {
   const { nodes, edges } = req.body;
 
   const g = new Graph({ directed: true });
 
-  nodes.forEach(node => g.setNode(node.id, node));
-  edges.forEach(edge => g.setEdge(edge.source, edge.target, edge));
+  nodes.forEach((node) => g.setNode(node.id, node));
+  edges.forEach((edge) => g.setEdge(edge.source, edge.target, edge));
 
   const isDAG = graphlib.alg.isAcyclic(g); // graphlib.alg is used here
 
